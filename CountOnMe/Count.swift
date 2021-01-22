ab5fd0e08e9395865d1db85b5890ade947257821
StephR <stephane.rihet@gmail.com>
2021-01-22 22:57:01 +0100

    Exportation et adaptation de la logique existante dans le ViewController pour le mettre dans la classe Count (Model) + Ajout d'un bouton AC pour la remise à zéro

--- CountOnMe/Count.swift
+++ CountOnMe/Count.swift
@@ -9,5 +9,47 @@
 import Foundation
 
 class Count {
-    <#code#>
+    // Error check computed variables
+    func expressionIsCorrect(elements: [String]) -> Bool {
+        return elements.last != "+" && elements.last != "-"
+    }
+    
+    func expressionHaveEnoughElement(elements: [String]) -> Bool {
+        return elements.count >= 3
+    }
+    
+    func canAddOperator(elements: [String]) -> Bool {
+        return elements.last != "+" && elements.last != "-"
+    }
+    
+    func expressionHaveResult(elements: [String]) -> Bool {
+        return elements.firstIndex(of: "=") != nil
+    }
+    
+    
+    func equal(elements: [String]) -> String! {
+        // Create local copy of operations
+        var operationsToReduce = elements
+        
+        // Iterate over operations while an operand still here
+        while operationsToReduce.count > 1 {
+            let left = Int(operationsToReduce[0])!
+            let operand = operationsToReduce[1]
+            let right = Int(operationsToReduce[2])!
+            
+            let result: Int
+            switch operand {
+            case "+": result = left + right
+            case "-": result = left - right
+            default: fatalError("Unknown operator !")
+            }
+            
+            operationsToReduce = Array(operationsToReduce.dropFirst(3))
+            operationsToReduce.insert("\(result)", at: 0)
+        }
+        return operationsToReduce.first
+    }
+    
+    
 }
+
